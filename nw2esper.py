fedc2af69dbc5eb10392c58feddd60651f51e5e8
Maximiliano Cittadini <shadkianash@gmail.com>
2017-09-27 12:10:26 -0300

    first commit

--- nw2esper.py
+++ nw2esper.py
@@ -5,20 +5,49 @@ import urllib2
 import json
 import time
 
-parser = argparse.ArgumentParser(prog='nw2esper',
-                                 description='Retrieve the metadata of the sessions afetedd by the query and format them to use in the Esper Tryout Page (http://esper-epl-tryout.appspot.com/epltryout/mainform.html)')
-parser.add_argument('-s', '--service', dest='service',
-                    help='the service that you want to use (e.g. -s http://broker:50103)', required=True)
-parser.add_argument('-q', '--query', dest='query',
-                    help='query used to retrieve the sessions of your interest (copied from NW investigation debug)',
-                    required=True)
+def fnobfuscate(s,mask):
+    nmask = [ord(c) for c in mask]
+    lmask = len(mask)
+    return ''.join([chr(ord(c) ^ nmask[i % lmask])
+                    for i, c in enumerate(s)])
+
+parser = argparse.ArgumentParser(prog='nw2esper',description='Retrieve the metadata of the sessions afected by the query and format them to use in the Esper Tryout Page (http://esper-epl-tryout.appspot.com/epltryout/mainform.html)')
+parser.add_argument('-s', '--service', dest='service',help='the service that you want to use (e.g. -s http://broker:50103)')
+parser.add_argument('-q', '--query', dest='query',help='query used to retrieve the sessions of your interest (copied from NW investigation debug)')
 parser.add_argument('-u', '--user', dest='username', help='the username to use in the REST API', default='admin')
-parser.add_argument('-p', '--password', dest='password', help='the password of the username to use in the REST API',
-                    default='netwitness')
-parser.add_argument('-o', '--output', dest='output',
-                    help='the output file. if is not specified, the output will be the stderr')
+parser.add_argument('-p', '--password', dest='password', help='the password of the username to use in the REST API',default='netwitness')
+parser.add_argument('-o', '--output', dest='output',help='the output file. if is not specified, the output will be the stderr')
+parser.add_argument('-O', '--obfuscate', dest='obfuscate',help='a coma separated list of the metakeys to obfuscate')
+parser.add_argument('-k', '--key', dest='key',help='the key used to (de)obfuscate data')
+parser.add_argument('-DO', '--deobfuscate', dest='deobfuscate',help='just deobfuscate using the key')
 
 args = parser.parse_args()
+
+if args.obfuscate != None:
+    obfuscate = 1
+    metaToobfuscate = args.obfuscate
+    if args.key != None:
+        OfKey = args.key
+    else:
+        sys.stderr.write ('You have to provide a key to obfuscate the results\n')
+        exit (1)
+else:
+    obfuscate = 0
+    if args.deobfuscate != None:
+        if args.key != None:
+            sys.stderr.write(fnobfuscate(args.deobfuscate.decode("hex"),args.key) + ' \n')
+            exit (0)
+        else:
+            sys.stderr.write('You have to provide a key to deobfuscate the results\n')
+            exit(1)
+
+if args.query == None:
+    sys.stderr.write('query must be set')
+    exit(1)
+
+if args.service == None:
+    sys.stderr.write('service must be set')
+    exit(1)
 
 rex = re.match("(?P<proto>https?)://(?P<service>[^:]+):(?P<port>\d+)", args.service.lower())
 if (rex != None):
@@ -36,6 +65,8 @@ if args.output != None:
     file = open (args.output, "w+")
 else:
     WriteToFile = 0
+
+
 
 sys.stderr.write('protocol:' + PROTOCOL + '\nservice: ' + SERVER + '\nport: ' + PORT + '\n')
 url_password = urllib2.HTTPPasswordMgrWithDefaultRealm()
@@ -100,7 +131,9 @@ try:
                         LastTime = CurrentTime
                         myEvent = 't=t.plus(' + str(TimeDelta) + ' seconds) \n' + myEvent
                         # MetaData['value'] = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(MetaData['value']))
-
+                if obfuscate == 1:
+                    if MetaData['type'].lower() in metaToobfuscate.lower():
+                        MetaData['value'] = fnobfuscate(MetaData['value'], OfKey).encode("hex")
                 MetaData['type'] = MetaData['type'].replace('.', '_')
                 mySchemaList[MetaData['type']] = MetaData['format']
                 if MetaData['format'] == 65:
@@ -109,6 +142,7 @@ try:
                     separator = '\''
                 else:
                     separator = ''
+
                 myEvent = myEvent + MetaData['type'] + '=' + separator + str(MetaData['value']) + separator + ', '
 
     myEvent = myEvent[:len(myEvent) - 2]
